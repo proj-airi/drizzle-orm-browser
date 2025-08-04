319eff69b5ea88523857b310433a1cd0ccc3c7ec
RainbowBird <git@luoling.moe>
2025-08-04 20:25:50 +0800

    feat: pg support

--- packages/drizzle-orm-browser-migrator/tsdown.config.ts
+++ packages/drizzle-orm-browser-migrator/tsdown.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   entry: {
     'index': './src/index.ts',
     'pglite/index': './src/pglite/index.ts',
+    'pg/index': './src/pg/index.ts',
   },
   unused: true,
   fixedExtension: true,
